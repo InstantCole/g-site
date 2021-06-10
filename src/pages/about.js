d00c5d5ffc65181fc5a058b6050501cfa10a8659
Cole Reichenberg <cole.reichenberg@gmail.com>
2021-06-09 20:50:00 -0600

    No Style yet!

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+import Layout from '../components/layout'
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='About Us'>
+      <h1>Learn about us here</h1>
+      <p>Time for more stuff here.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+
+// markup
+const IndexPage = () => {
+  return (
+    <Layout pageTitle='Home'>
+      <h1>Welcome to my Gatsby site!</h1>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+    
+  )
+}
+
+export default IndexPage
